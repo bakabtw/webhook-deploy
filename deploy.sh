0088bcef39353e676df2bb3f88bd56576e74f45d
Eugene Konstantinov <me@knst.me>
2023-07-14 14:42:50 +0300

    Rewritten deploy.sh for connecting via SSH

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,35 @@
 #!/usr/bin/env sh
 
+SSH_KEY_FILE=/run/secrets/ssh_key
+
 # Checking ENV variables
 if [ -z "$DOCKER_DIR" ]; then
     echo "DOCKER_DIR variable is not set"
+    exit
 fi
 
-# Checking DOCKER_HOST
-if [ -z "$DOCKER_HOST" ]; then
-    echo "DOCKER_HOST variable is not set"
+if [ -z "$SSH_HOST" ] || [ -z "$SSH_PORT" ] || [ -z "$SSH_USERNAME" ]; then
+    echo "SSH credentials are not fully provided. You should provide SSH_HOST, SSH_PORT, SSH_USERNAME"
+    exit
 fi
 
-# Checking existence of docker directory
+# Checking presence of SSH_KEY secret
+if ! [ -f "$SSH_KEY_FILE" ]; then
+    echo "SSH_KEY secret does not exist. Please create a secret:"
+    echo "printf \"This is a secret\" | docker secret create ssh_key -"
+    exit
+fi
+
+# Connecting via SSH to remote server
+ssh -o StrictHostKeyChecking=no -i "$SSH_KEY_FILE" "$SSH_USERNAME"@"$SSH_HOST" -p"$SSH_PORT" << EOF
 if ! [ -d "$DOCKER_DIR" ]; then
   echo "$DOCKER_DIR does not exist"
+  exit
 fi
 
-# Changing dir
 cd "$DOCKER_DIR" || exit
 
-# Pulling updates from git repository
-# git pull
-
-# Pulling docker images
-docker-compose -H "tcp://$DOCKER_HOST" pull
-
-# Restarting updated containers
-docker-compose -H "tcp://$DOCKER_HOST" up -d
+git pull
+docker-compose pull
+docker-compose up -d
+EOF
